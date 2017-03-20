4d0900c6e6fabec742b22ba41601e3a808a9e04f
AdamDWalker <adamwalker03@googlemail.com>
2017-03-20 12:23:44 +0000

    Colour finding mostly works
    - Added colour ranges for each colour
    - Checking works
    - Yellow always gets checked for some reason
    - Need to fix the movement thing where it immediately checks the thing as found then stops moving

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 
 class Follower:
-    
+
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
         cv2.namedWindow("window", 1)
@@ -24,47 +24,67 @@ class Follower:
         self.cmd_vel_pub = rospy.Publisher('/turtlebot/cmd_vel',
                                            Twist, queue_size=1)
         self.twist = Twist()
-        
-        self.velocity = 1
+
+        self.velocity = 0.5
         self.colourTarget = 0
         self.colourFound = [0,0,0,0]
 
     def image_callback(self, msg):
         image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        
+
         # Yellow
         if( self.colourTarget == 0 and self.colourFound[0] == 0):
+            lower_bound = numpy.array([ 30, 200,  100])
+            upper_bound = numpy.array([ 50, 255, 150])
+        # Green
+        elif( self.colourTarget == 1 and self.colourFound[1] == 0):
             lower_bound = numpy.array([ 40, 100,  80])
             upper_bound = numpy.array([ 80, 255, 250])
-        # Green
-        elif( self.colourTarget == 0 and self.colourFound[0] == 0):
-            lower_bound = numpy.array([ 40, 100,  80])
-            upper_bound = numpy.array([ 80, 255, 250])            
         # Blue
-        elif( self.colourTarget == 0 and self.colourFound[0] == 0):
-            lower_bound = numpy.array([ 40, 100,  80])
-            upper_bound = numpy.array([ 80, 255, 250])           
+        elif( self.colourTarget == 2 and self.colourFound[2] == 0):
+            lower_bound = numpy.array([ 100, 50,  50])
+            upper_bound = numpy.array([ 130, 255, 255])
         # Red
-        elif( self.colourTarget == 0 and self.colourFound[0] == 0):
-            lower_bound = numpy.array([ 40, 100,  80])
-            upper_bound = numpy.array([ 80, 255, 250])           
-        # Otherwise just set the mask to black
+        elif( self.colourTarget == 3 and self.colourFound[3] == 0):
+            lower_bound = numpy.array([ 0, 200, 100])
+            upper_bound = numpy.array([ 0, 255, 150])
+        # Otherwise just set the mask to white
         else:
-            lower_bound = numpy.array([ 0, 0,  0])
-            upper_bound = numpy.array([ 0, 0, 0])
-        
+            lower_bound = numpy.array([ 255, 255, 255])
+            upper_bound = numpy.array([ 255, 255, 255])
+
         mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
         h, w, d = image.shape
 
         M = cv2.moments(mask)
-        
+
         if M['m00'] > 0:
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-            
+
+            if(self.colourTarget == 0):
+                self.colourFound[0] = 1
+                self.colourTarget = (self.colourTarget+1)%4
+                print("Yellow Found")
+
+            elif(self.colourTarget == 1):
+                self.colourFound[1] = 1
+                self.colourTarget = (self.colourTarget+1)%4
+                print("Green Found")
+
+            elif(self.colourTarget == 2):
+                self.colourFound[2] = 1
+                self.colourTarget = (self.colourTarget+1)%4
+                print("Blue Found")
+
+            elif(self.colourTarget == 3):
+                self.colourFound[3] = 1
+                self.colourTarget = (self.colourTarget+1)%4
+                print("Red Found")
+
             # BEGIN CONTROL
             err = cx - w/2
             self.twist.linear.x = self.velocity
@@ -72,7 +92,7 @@ class Follower:
             self.cmd_vel_pub.publish(self.twist)
             # END CONTROL
         else:
-            self.colourTarget = (self.colourTarget + 1) % 4
+            self.colourTarget = (self.colourTarget+1)%4
         cv2.imshow("window", image)
         cv2.imshow("mask", mask)
         cv2.waitKey(1)
